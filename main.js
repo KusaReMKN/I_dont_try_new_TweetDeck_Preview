5d4bfcdcc6523798eb80892f17301ff6a9517ff7
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2023-07-13 22:34:21 +0900

    Fix to run only on the legacy TweetDeck

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const targetClassName = 'js-gryphon-beta-btn gryphon-beta-btn-container';
+const cookieString = 'tweetdeck_version=legacy';
 
-const timerId = setInterval(() => {
-	const elems = document.getElementsByClassName(targetClassName);
-	const banner = elems.item(0);
-	if (elems.length > 0) {
+if (document.cookie.indexOf(cookieString) !== -1) {
+	const timerId = setInterval(() => {
+		const elems = document.getElementsByClassName(targetClassName);
 		const banner = elems.item(0);
-		banner.parentElement.removeChild(banner);
-		clearInterval(timerId);
-	}
-});
+		if (elems.length > 0) {
+			const banner = elems.item(0);
+			banner.parentElement.removeChild(banner);
+			clearInterval(timerId);
+		}
+	});
+}
